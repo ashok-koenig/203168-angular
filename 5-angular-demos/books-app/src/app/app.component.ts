e8747e8cc04c2e3b138fe5c2c8e38f40c6079fe2
Ashok S <ashok.s@koenig-solutions.com>
2024-11-20 13:29:42 +0530

    list the books in table

--- 5-angular-demos/books-app/src/app/app.component.ts
+++ 5-angular-demos/books-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddBookComponent } from './add-book/add-book.component';
+import { ListBookComponent } from './list-book/list-book.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddBookComponent, ListBookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 5-angular-demos/books-app/src/app/list-book/list-book.component.ts
+++ 5-angular-demos/books-app/src/app/list-book/list-book.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { BookService } from '../services/book.service';
+import { Book } from '../models/book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-book',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-book.component.html',
   styleUrl: './list-book.component.css'
 })
 export class ListBookComponent {
-
+  books: Book[] = []
+  constructor(private bookService: BookService){
+    this.books = bookService.getBooks()
+  }
 }
