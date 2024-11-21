4aebbd3db6ac3521b051ff6af04dea438a5e6fe0
Ashok S <ashok.s@koenig-solutions.com>
2024-11-21 11:29:19 +0530

    profile details

--- 5-angular-demos/routes-demo-app/src/app/pages/profile-detail/profile-detail.component.ts
+++ 5-angular-demos/routes-demo-app/src/app/pages/profile-detail/profile-detail.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-detail.component.html',
   styleUrl: './profile-detail.component.css'
 })
 export class ProfileDetailComponent {
-
+  profileName: string = ''
+  picUrl: string =''
+  constructor(private route: ActivatedRoute){
+    this.profileName = route.snapshot.params['name']
+    this.picUrl = `images/${this.profileName}.jpg`
+  }
 }
