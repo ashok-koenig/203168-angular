673be5dafdd564afb808f12195d9be2bbbd4684a
Ashok S <ashok.s@koenig-solutions.com>
2024-11-21 12:12:44 +0530

    query params

--- 5-angular-demos/routes-demo-app/src/app/pages/profile-detail/profile-detail.component.ts
+++ 5-angular-demos/routes-demo-app/src/app/pages/profile-detail/profile-detail.component.ts
@@ -13,7 +13,8 @@ export class ProfileDetailComponent {
   profileName: string = ''
   picUrl: string =''
   constructor(private route: ActivatedRoute){
-    this.profileName = route.snapshot.params['name']
-    this.picUrl = `images/${this.profileName}.jpg`
+    this.profileName = route.snapshot.queryParams['firstName'] + route.snapshot.queryParams['lastName']
+   const urlName:string = route.snapshot.params['name']
+    this.picUrl = `images/${urlName}.jpg`
   }
 }
