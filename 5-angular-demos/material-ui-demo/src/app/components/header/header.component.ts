5c5de810a31f940faf39d4ed5326a1cc738c5378
Ashok S <ashok.s@koenig-solutions.com>
2024-11-26 13:23:02 +0530

    responsive toolbar buttons

--- 5-angular-demos/material-ui-demo/src/app/components/header/header.component.ts
+++ 5-angular-demos/material-ui-demo/src/app/components/header/header.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout'
+import { MatMenuModule} from '@angular/material/menu'
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, RouterLink, RouterLinkActive, MatButtonModule],
+  imports: [MatToolbarModule, RouterLink, RouterLinkActive, MatButtonModule, FlexLayoutModule, MatMenuModule, MatIconModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
