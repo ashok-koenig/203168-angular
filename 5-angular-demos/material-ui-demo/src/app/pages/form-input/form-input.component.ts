7cf5f305742d2ee62c2c5b561e6266491fb94bce
Ashok S <ashok.s@koenig-solutions.com>
2024-11-26 12:09:30 +0530

    radio and select option

--- 5-angular-demos/material-ui-demo/src/app/pages/form-input/form-input.component.ts
+++ 5-angular-demos/material-ui-demo/src/app/pages/form-input/form-input.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-form-input',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, FormsModule, MatRadioModule, MatSelectModule],
   templateUrl: './form-input.component.html',
   styleUrl: './form-input.component.css'
 })
 export class FormInputComponent {
-
+  firstName = ''
+  lastName = ''
+  email = ''
+  message = ''
+  gender = 'male'
+  course =''
 }
