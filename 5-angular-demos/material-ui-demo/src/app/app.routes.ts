5ee266bbc9de45f4846690e93a7f57b3e732cd11
Ashok S <ashok.s@koenig-solutions.com>
2024-11-26 11:05:26 +0530

    simple material button

---
+++ 5-angular-demos/material-ui-demo/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ButtonDemoComponent } from './pages/button-demo/button-demo.component';
+
+export const routes: Routes = [
+    {path:'', component: ButtonDemoComponent}
+];
---
+++ 5-angular-demos/material-ui-demo/src/app/pages/button-demo/button-demo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'
+
+@Component({
+  selector: 'app-button-demo',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './button-demo.component.html',
+  styleUrl: './button-demo.component.css'
+})
+export class ButtonDemoComponent {
+
+}
