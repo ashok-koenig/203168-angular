e6433d9d2640e68eec5dae6e2d4c3cf67efd2544
Ashok S <ashok.s@koenig-solutions.com>
2024-11-26 12:50:43 +0530

    card demo code

--- 5-angular-demos/material-ui-demo/src/app/app.routes.ts
+++ 5-angular-demos/material-ui-demo/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { ButtonDemoComponent } from './pages/button-demo/button-demo.component';
 import { FormInputComponent } from './pages/form-input/form-input.component';
 import { SnackbarDemoComponent } from './pages/snackbar-demo/snackbar-demo.component';
 import { DialogDemoComponent } from './pages/dialog-demo/dialog-demo.component';
+import { CardDemoComponent } from './pages/card-demo/card-demo.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/button', pathMatch: 'full'},
     {path:'button', component: ButtonDemoComponent},
     {path: 'form', component: FormInputComponent},
     {path: 'snackbar', component: SnackbarDemoComponent},
-    {path: 'dialog', component: DialogDemoComponent}
+    {path: 'dialog', component: DialogDemoComponent},
+    {path: 'card', component: CardDemoComponent}
 ];
