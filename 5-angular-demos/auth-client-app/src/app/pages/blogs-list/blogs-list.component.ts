581f853d91456669a357203d15c883c5afc9b9e3
Ashok S <ashok.s@koenig-solutions.com>
2024-11-27 11:13:24 +0530

    blogs list in ui

--- 5-angular-demos/auth-client-app/src/app/pages/blogs-list/blogs-list.component.ts
+++ 5-angular-demos/auth-client-app/src/app/pages/blogs-list/blogs-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { BlogService } from '../../services/blog.service';
 import { Blog } from '../../models/blog.model';
+import {MatCardModule} from '@angular/material/card'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blogs-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './blogs-list.component.html',
   styleUrl: './blogs-list.component.css'
 })
