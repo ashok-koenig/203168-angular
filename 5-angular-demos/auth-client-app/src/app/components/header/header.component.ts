3f3a66d9a22f4a4a8360e568ee977387c63dd567
Ashok S <ashok.s@koenig-solutions.com>
2024-11-27 12:40:34 +0530

    with logout

--- 5-angular-demos/auth-client-app/src/app/components/header/header.component.ts
+++ 5-angular-demos/auth-client-app/src/app/components/header/header.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatButtonModule} from '@angular/material/button'
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { TokenService } from '../../services/token.service';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -10,5 +12,19 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  userRole: string =''
+  isLoggedIn: boolean = false
+  constructor(private authService: AuthService, private router: Router, private tokenService: TokenService){
+    tokenService.isLoggedIn.subscribe((value)=>{
+      this.isLoggedIn = value
+    })
+    tokenService.userRole.subscribe((value)=>{
+      this.userRole = value
+    })
 
+  }
+  handleLogout(){
+    this.authService.logout();
+    this.router.navigate(['/login'])
+  }
 }
