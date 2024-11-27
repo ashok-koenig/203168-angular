b6ceaf9156fbec9dfe9d720b96b687894b406af4
Ashok S <ashok.s@koenig-solutions.com>
2024-11-27 10:47:43 +0530

    auth and blog components and services

---
+++ 5-angular-demos/auth-client-app/src/app/pages/blogs-list/blogs-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogs-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './blogs-list.component.html',
+  styleUrl: './blogs-list.component.css'
+})
+export class BlogsListComponent {
+
+}
