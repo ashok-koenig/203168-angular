25c401bf7db098c7b9741ff68b5f0d1038d69bea
Ashok S <ashok.s@koenig-solutions.com>
2024-11-27 12:18:32 +0530

    login ts updated

--- 5-angular-demos/auth-client-app/src/app/pages/login/login.component.ts
+++ 5-angular-demos/auth-client-app/src/app/pages/login/login.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { TokenService } from '../../services/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  myForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required])
+  })
+  error: string = ''
+  constructor(private authService: AuthService, private tokenService: TokenService, private router: Router){
+    if(tokenService.isLoggedIn.value){
+      router.navigate(['/blogs'])
+    }
+  }
+  handleSubmit(){
+    this.error =''
+    const email = this.myForm.value.email
+    const password = this.myForm.value.password
+    if(email && password){
+      this.authService.login({email, password}).subscribe({
+        next: (response)=>{
+          if(response && response.token){
+            this.router.navigate(['/blogs'])
+          }
+        },
+        error:(error)=>{
+          this.error = error.message
+        }
+      })
+    }
+    
+  }
 }
--- 5-angular-demos/auth-client-app/src/app/services/auth.service.ts
+++ 5-angular-demos/auth-client-app/src/app/services/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { TokenService } from './token.service';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  apiBaseUrl = 'http://localhost:3000'
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
 
-  constructor() { }
+  signup(data: User){
+    return this.http.post<any>(`${this.apiBaseUrl}/signup`, data)
+  }
+  login(data: User){
+    return this.http.post<any>(`${this.apiBaseUrl}/login`, data)
+                          .pipe(map((response)=>{
+                            if(response.token){
+                              this.tokenService.setToken(response.token, response.role)
+                            }
+                            return response;
+                          }))
+  }
+  logout(){
+    this.tokenService.removeToken()
+  }
 }
