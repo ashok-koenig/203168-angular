427a25a56bcf518d73d0389058bbd181f6544290
Ashok S <ashok.s@koenig-solutions.com>
2024-11-27 13:23:28 +0530

    sending token with post request

--- 5-angular-demos/auth-client-app/src/app/pages/blog-form/blog-form.component.ts
+++ 5-angular-demos/auth-client-app/src/app/pages/blog-form/blog-form.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { BlogService } from '../../services/blog.service';
+import { Router } from '@angular/router';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout'
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-blog-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FlexLayoutModule, MatCardModule, MatButtonModule, MatInputModule],
   templateUrl: './blog-form.component.html',
   styleUrl: './blog-form.component.css'
 })
 export class BlogFormComponent {
-
+  blogForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    content: new FormControl('', Validators.required)
+  })
+  error: string =''
+  constructor(private blogService: BlogService, private router: Router){}
+  handleSubmit(){
+    this.error = ''
+    const title = this.blogForm.value.title
+    const content = this.blogForm.value.content
+    if(title && content){
+      this.blogService.createBlog({title, content}).subscribe({
+        next:(response)=>{
+          if(response){
+            this.router.navigate(['/blogs'])
+          }
+        },
+        error: (error)=>{
+          this.error = error;
+        }
+      })
+    }
+  }
 }
--- 5-angular-demos/auth-client-app/src/app/services/blog.service.ts
+++ 5-angular-demos/auth-client-app/src/app/services/blog.service.ts
@@ -1,15 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Blog } from '../models/blog.model';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
   apiBaseUrl = "http://localhost:3000/blogs"
-  constructor(private http: HttpClient) { }
+  token = ''
+  constructor(private http: HttpClient, private tokenService: TokenService) { 
+    this.token = tokenService.getToken()
+  }
   getBlogs():Observable<Blog[]>{
     return this.http.get<any>(this.apiBaseUrl)
+  }
+  createBlog(data: Blog){
+    const headers = new HttpHeaders({
+      'authorization': 'Bearer '+ this.token
+    })
+    return this.http.post<any>(this.apiBaseUrl, data, {headers})
   }
 }
